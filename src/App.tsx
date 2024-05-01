9365d2548a15f7b74753b29d9a0858cea5d6ea8d
developerhimanshu <chamyalhemsingh@gmail.com>
2024-05-01 09:16:44 +0530

    Adding the front-end of the the ecommerce application

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Inspect from 'inspx'
+const App = () => {
+  return (
+    <div className="bg-aquamarine">
+      <Inspect>
+          <h1 className="text-silver">Hoodify</h1> 
+          <h1 className="text-marine-blue">Hoodify</h1> 
+          <h1 className="text-red">Hoodify</h1> 
+          <h1 className="text-periwinkle">Hoodify</h1> 
+          <h1 className="text-silver">Hoodify</h1> 
+      </Inspect>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "marine-blue":"#314899",
+        "raisin-black":"#231F20",
+        silver:"#C2C2C2",
+        "dim-gray":"#737373",
+        "floral-white":"#F8F4EC",
+        "white-smoke":"#F3F3F3",
+        "sea-salt":"#FAFAFA",
+        "periwinkle":"#C7DBFF",
+        "voilet-blue":"#4243B1",
+        "voilet-blue-2":"#3738A6",
+        aquamarine: "#B2FCE4",
+        red:"#DF1841",
+      }
+    },
+  },
+  plugins: [],
+}
